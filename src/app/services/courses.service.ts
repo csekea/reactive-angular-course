d04609c97146c33291df977758cd098321cd13c7
csekea <anna.cseke@gmail.com>
2025-08-23 18:01:23 +0200

    add search lesson component

--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { map, shareReplay } from 'rxjs/operators';
+import { filter, map, shareReplay } from 'rxjs/operators';
 import { Course } from '../model/course';
+import { Lesson } from '../model/lesson';
 
 @Injectable({
     providedIn: 'root'
@@ -42,5 +43,18 @@ export class CoursesService {
 
     deleteCourse(id: number): Observable<void> {
         return this.http.delete<void>(`${this.apiUrl}/${id}`);
+    }
+
+    searchLessons(search: string): Observable<Lesson[]>{
+        return this.http.get<Lesson[]>("/api/lessons", {
+            params: {
+                filter: search,
+                pageSize: "100"
+            }
+        })
+        .pipe(
+            map(res => res["payload"]),
+            shareReplay()
+        )
     }
 }
